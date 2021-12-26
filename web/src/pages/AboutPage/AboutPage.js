3c9c1e6611a5629d92e54a051d5ea0a5a05558ea
Diahron Grismore <diahron@gmail.com>
2021-12-26 15:20:10 -0600

    Initial Files
    original files from RedwoodJS tutorial I.

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <>
+      <h1>AboutPage</h1>
+      <p>
+        This site was created to demonstrate my mastery <em>of</em> Redwood:
+        Look on my works, ye mighty, and despair!
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
